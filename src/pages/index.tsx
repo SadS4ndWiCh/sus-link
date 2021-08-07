4b67ba444f545ec4d41eeeab6dd0784e478e6af6
SadS4ndWiCh <caiocamargo007@gmail.com>
2021-08-07 19:34:56 -0300

    add: others metatags

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,17 +12,20 @@ export default function Home() {
       <Head>
         <title>SusLink</title>
 
+        {/* Primary Meta Tags */}
         <meta name="title" content="SusLink" />
         <meta name="description" content="Não são gerados links curtos, apenas um link de aparência suspeita" />
 
+        {/* Open Graph / Facebook */}
         <meta property="og:type" content="website" />
-        {/* <meta property="og:url" content="https://metatags.io/" /> */}
+        <meta property="og:url" content="https://suslink.vercel.app/" />
         <meta property="og:title" content="SusLink" />
         <meta property="og:description" content="Não são gerados links curtos, apenas um link de aparência suspeita" />
         <meta property="og:image" content="suslink_image.png" />
 
+        {/* Twitter */}
         <meta property="twitter:card" content="summary_large_image" />
-        {/* <meta property="twitter:url" content="https://metatags.io/" /> */}
+        <meta property="twitter:url" content="https://suslink.vercel.app/" />
         <meta property="twitter:title" content="SusLink" />
         <meta property="twitter:description" content="Não são gerados links curtos, apenas um link de aparência suspeita" />
         <meta property="twitter:image" content="suslink_image.png" />
