aff7d56b0898dbfd8dafce1594fcacd27fec7b26
SadS4ndWiCh <caiocamargo007@gmail.com>
2021-08-08 19:02:37 -0300

    add: 404 error page

---
+++ src/pages/404.tsx
@@ -0,0 +1,31 @@
+import Head from 'next/head';
+import Link from 'next/link';
+
+import styles from '@styles/pages/PageNotFound.module.scss';
+
+export default function PageNotFound() {
+  return (
+    <div className={styles.pageNotFoundContainer}>
+      <Head>
+        <title>Suslink - Página não encontrada</title>
+      </Head>
+
+      <div>
+        <div>
+          <span>4</span>
+          <span>0</span>
+          <span>4</span>
+        </div>
+        <h1>Página não encontrada</h1>
+      </div>
+      
+      <div>
+        <p>
+          Aproveitando que está aqui, deseja criar algum link suspeito e trolar amiguinhos? 
+          Vá para página inicial e crie um link :)
+        </p>
+        <Link href='/'><a>Quero criar</a></Link>
+      </div>
+    </div>
+  )
+}
