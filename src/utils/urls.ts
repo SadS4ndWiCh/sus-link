59eaa79667b5138b860e54d0aaacf568ba285faf
SadS4ndWiCh <caiocamargo007@gmail.com>
2022-08-05 20:18:10 -0300

    add generate sus link function

--- src/utils/urls.ts
+++ src/utils/urls.ts
@@ -1,29 +1,47 @@
-import ShortUniqueId from 'short-unique-id';
+const suspiciousContent = {
+  animals: ["aardvark","albatross","alligator","alpaca","ant","anteater","antelope","ape","armadillo","donkey","baboon","badger","barracuda","bat","bear","beaver","bee","bison","boar","buffalo","butterfly","camel","capybara","caribou","cassowary","cat","caterpillar","cattle","chamois","cheetah","chicken","chimpanzee","chinchilla","chough","clam","cobra","cockroach","cod","cormorant","coyote","crab","crane","crocodile","crow","curlew","deer","dinosaur","dog","dogfish","dolphin","dotterel","dove","dragonfly","duck","dugong","dunlin","eagle","echidna","eel","eland","elephant","elk","emu","falcon","ferret","finch","fish","flamingo","fly","fox","frog","gaur","gazelle","gerbil","giraffe","gnat","gnu","goat","goldfinch","goldfish","goose","gorilla","goshawk","grasshopper","grouse","guanaco","gull","hamster","hare","hawk","hedgehog","heron","herring","hippopotamus","hornet","horse","human","hummingbird","hyena","ibex","ibis","jackal","jaguar","jay","jellyfish","kangaroo","kingfisher","koala","kookabura","kouprey","kudu","lapwing","lark","lemur","leopard","lion","llama","lobster","locust","loris","louse","lyrebird","magpie","mallard","manatee","mandrill","mantis","marten","meerkat","mink","mole","mongoose","monkey","moose","mosquito","mouse","mule","narwhal","newt","nightingale","octopus","okapi","opossum","oryx","ostrich","otter","owl","oyster","panther","parrot","partridge","peafowl","pelican","penguin","pheasant","pig","pigeon","pony","porcupine","porpoise","quail","quelea","quetzal","rabbit","raccoon","rail","ram","rat","raven","red deer","red panda","reindeer","rhinoceros","rook","salamander","salmon","sand dollar","sandpiper","sardine","scorpion","seahorse","seal","shark","sheep","shrew","skunk","snail","snake","sparrow","spider","spoonbill","squid","squirrel","starling","stingray","stinkbug","stork","swallow","swan","tapir","tarsier","termite","tiger","toad","trout","turkey","turtle","viper","vulture","wallaby","walrus","wasp","weasel","whale","wildcat","wolf","wolverine","wombat","woodcock","woodpecker","worm","wren","yak","zebra"],
+  adjectives: ['beautiful', 'cutie', 'nice'],
+  videoExtensions: ['.mp4', '.avi', '.mov', '.mkv'],
+  archiveExtensions: ['.exe', '.bin', '.dll', '.dam', '.dr', '.plugin', '.kit', '.gen'],
+  sexualWords: ['hot', 'sexy'],
+  games: ['league-of-legends', 'roblox', 'minecraft', 'valorant', 'terraria'],
+  virusTypes: ['adware', 'backdoor', 'behavior', 'browsermodifier', 'constructor', 'ddos', 'exploit', 'trojan'],
+  virusNames: ['iloveyou', 'troia', 'aids-trojan', 'wannacry', 'petya'],
+  domainExtensions: ['.com', '.com.ru', '.onion', '.jp', '.fr', '.xxx']
+};
 
-const suspects = [
-  '5wayskillwithmelon',
-  'dhiuwesn2nver4osdoiv.onion',
-  'virustroia.exe',
-  'plshackme',
-  'notisv1rus',
-  'howkillants',
-  'webcamaccess.bin',
-  'leagueoflegendshack',
-  'sexyapples',
-];
+const suspiciousFormats = [
+  'sexualWords-animals-videoExtensions-virusTypes-archiveExtensions',
+  'games-virusTypes-virusNames-archiveExtensions',
+  
+]
 
-export function generateSuspectUrl() {
-  const uid = new ShortUniqueId();
+export const generateSusLink = () => {
+  const choosedFormat = randomIndex(suspiciousFormats);
 
-  const suspectIndex = Math.floor(Math.random() * suspects.length);
-  const suspectName = suspects[suspectIndex];
+  return generateSusLinkByFormat(choosedFormat);
+};
 
-  return `${uid.stamp(10)}_${suspectName}`;
+const generateSusLinkByFormat = (linkFormat: string) => {
+  const linkStructure = linkFormat.split('-');
 
-}
+  let suspiciousLink = '';
+  linkStructure.forEach((instruction) => {
+    const content = suspiciousContent[instruction as keyof typeof suspiciousContent];
 
-export function convertUrl(suspectUrl: string) {
-  const { protocol, hostname, port } = window.location;
+    suspiciousLink += randomIndex(content);
+  });
 
-  return `${protocol}//${hostname}${port ? ':' + port : ''}/${suspectUrl}`;
-}
+  return suspiciousLink;
+};
+
+const randomProperty = (obj: any) => {
+  const keys = Object.keys(obj) as string[];
+  const choosedKey = keys[keys.length * Math.random() << 0];
+
+  return obj[choosedKey];
+};
+
+const randomIndex = (arr: any[]) => {
+  return arr[arr.length * Math.random() << 0];
+};
