8e1773e8e3bdf38d0c8c87a6b3b372b7589c671b
SadS4ndWiCh <caiocamargo007@gmail.com>
2022-08-05 20:12:01 -0300

    add container configurations

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,17 @@ module.exports = {
   content: ["src/**/*.tsx"],
   theme: {
     extend: {},
-  },
-  plugins: [],
-};
 
+    container: {
+      padding: "1rem",
+      center: true,
+      screens: {
+        sm: "100%",
+        md: "100%",
+        lg: "640px",
+        xl: "640px",
+      },
+    },
+  },
+  plugins: [require("@tailwindcss/forms")],
+};
