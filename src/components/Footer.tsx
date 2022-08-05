1f5d3dd4e7aad24772df5ad484b090d0e3e317fe
SadS4ndWiCh <caiocamargo007@gmail.com>
2022-08-05 20:16:03 -0300

    add footer component

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,12 +1,18 @@
-import styles from '@styles/components/Footer.module.scss';
-
-export function Footer() {
+export const Footer = () => {
   return (
-    <footer className={styles.footerContainer}>
+    <footer className='flex flex-col gap-4 py-4 border-t text-sm border-zinc-400'>
       <p>
-        O objetivo desse site é apenas por entreterimento. Não nos responsabilizamos caso alguém
-        vincule a um endereço malicioso, logo vai por sua conta e risco.
+        O objetivo desse site é apenas entreterimento, os links gerados não contém nada de malicioso. Mas não nos 
+        responsabilizamos se alguém vincular a link realmente malicioso.
       </p>
+
+      <div className='flex items-center justify-between text-zinc-700'>
+        <span>© 2021 - 2022 </span>
+        <span>
+          Desenvolvido por {' '}
+          <a className='text-zinc-900 underline underline-offset-2 hover:text-zinc-700' href="https://caiovinicius.vercel.app">Caio Vinícius</a>
+        </span>
+      </div>
     </footer>
   )
-}
+};
