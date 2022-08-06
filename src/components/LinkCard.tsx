1469e05c318f246f01419afc8d1328c893d9280a
SadS4ndWiCh <caiocamargo007@gmail.com>
2022-08-06 12:37:41 -0300

    add responsivity and remove link button

--- src/components/LinkCard.tsx
+++ src/components/LinkCard.tsx
@@ -1,14 +1,15 @@
-import { Clipboard } from "phosphor-react";
+import { Clipboard, Trash } from "phosphor-react";
 import { toast } from "react-toastify";
 import { useCopyToClipboard } from "usehooks-ts";
 import { ISusLink } from "./CreateLinkForm";
 
 type Props = {
   data: ISusLink;
+  onRemoveLink: (linkId: string) => void;
 };
 
-export const LinkCard = ({ data }: Props) => {
-  const [value, copy] = useCopyToClipboard();
+export const LinkCard = ({ data, onRemoveLink }: Props) => {
+  const [_, copy] = useCopyToClipboard();
 
   const createdAt = new Date(data.created_at);
   const createAtFormatted = createdAt.toLocaleDateString();
@@ -25,19 +26,29 @@ export const LinkCard = ({ data }: Props) => {
   };
 
   return (
-    <div className='flex items-center justify-between p-4 border border-zinc-400 rounded-md'>
-      <div className='flex flex-col gap-1'>
+    <div className='flex flex-col justify-between gap-2 p-4 border border-zinc-400 rounded-md sm:flex-row sm:items-center'>
+      <div className='flex flex-col gap-1 overflow-clip'>
         <time dateTime={createdAt.toString()} className='text-xs text-zinc-600'>{ createAtFormatted }</time>
-        <span className='text-sm'>{ data.original_url }</span>
-        <span className='font-semibold'>{ data.sus_url }</span>
+        <span className='text-sm truncate'>{ data.original_url }</span>
+        <span className='font-semibold truncate'>{ data.sus_url }</span>
       </div>
 
-      <button
-        onClick={handleCopyToClipboard}
-        className='h-fit p-2 border border-zinc-400 rounded-md transition-colors hover:bg-zinc-300'
-      >
-        <Clipboard size={24} />
-      </button>
+      <div className='flex w-full gap-1 sm:flex-col sm:items-center sm:w-auto'>
+        <button
+          onClick={handleCopyToClipboard}
+          title='Copiar link'
+          className='flex-1 flex justify-center p-2 border border-zinc-400 rounded-md transition-colors hover:bg-zinc-300 sm:h-fit'
+        >
+          <Clipboard size={24} />
+        </button>
+        <button
+          onClick={() => onRemoveLink(data.id)}
+          title='Remover dos meus links'
+          className='flex-1 flex justify-center p-2 border border-zinc-400 rounded-md text-zinc-100 bg-red-600 transition-colors hover:bg-red-500 sm:h-fit'
+        >
+          <Trash size={24} />
+        </button>
+      </div>
     </div>
   )
 };
