9012b3a911ee30e53e99e064d3faf64fd341747d
SadS4ndWiCh <caiocamargo007@gmail.com>
2022-08-06 12:36:53 -0300

    add resposivity, loading button and id in link data

--- src/components/CreateLinkForm.tsx
+++ src/components/CreateLinkForm.tsx
@@ -1,7 +1,10 @@
 import { FormEvent } from "react";
-import { useLocalStorage } from "usehooks-ts";
+import { useBoolean, useLocalStorage } from "usehooks-ts";
+import ShortUniqueId from "short-unique-id";
 
 import { generateSusLink } from "@utils/urls";
+import { addNewLink } from "@utils/database";
+import { SpinnerGap } from "phosphor-react";
 
 interface CreateLinkFormEvent extends FormEvent<HTMLFormElement> {
   target: HTMLFormElement & {
@@ -10,6 +13,7 @@ interface CreateLinkFormEvent extends FormEvent<HTMLFormElement> {
 };
 
 export interface ISusLink {
+  id: string;
   original_url: string;
   sus_url: string;
   created_at: number;
@@ -19,46 +23,59 @@ export const GENERATED_LINKS_KEY = '@suslink:generated_links';
 
 export const CreateLinkForm = () => {
   const [generatedLinks, setGeneratedLinks] = useLocalStorage<ISusLink[]>(GENERATED_LINKS_KEY, []);
+  const { value: isLoading, setTrue: setLoadingStart, setFalse: setLoadingEnd } = useBoolean(false);
 
-  const handleCreateLink = (e: CreateLinkFormEvent) => {
+  const handleCreateLink = async (e: CreateLinkFormEvent) => {
     e.preventDefault();
+    setLoadingStart();
 
     const original_url = e.target.url.value;
     const sus_url = `https://suslink.vercel.app/${generateSusLink()}`;
+    const uid = new ShortUniqueId({ length: 10 });
+    const newLinkData = {
+      id: uid(),
+      original_url,
+      sus_url,
+      created_at: Date.now(),
+    };
+
+    const result = await addNewLink(newLinkData, true);
 
     setGeneratedLinks([
       ...generatedLinks,
-      {
-        original_url,
-        sus_url,
-        created_at: Date.now(),
-      }
+      newLinkData
     ])
 
     e.target.url.value = '';
+
+    setLoadingEnd();
   };
 
   return (
-    <form onSubmit={handleCreateLink} className='flex items-end justify-between gap-2 py-6'>
-      <div className='flex-1 flex flex-col'>
-        <label htmlFor='url'>url</label>
-        <input
-          id='url'
-          type='url'
-          name='url'
-          placeholder='https://example.com'
-          autoComplete='off'
-          className='rounded-md border-zinc-400 bg-transparent focus:ring-zinc-900 focus:border-zinc-900'
-          required
-        />
-      </div>
+    <form onSubmit={handleCreateLink}>
+      <div className='flex flex-col justify-between gap-2 py-6 sm:flex-row sm:items-end'>
+        <div className='flex-1 flex flex-col'>
+          <label htmlFor='url'>url</label>
+          <input
+            id='url'
+            type='url'
+            name='url'
+            placeholder='https://example.com'
+            autoComplete='off'
+            className='rounded-md border-zinc-400 bg-transparent focus:ring-zinc-900 focus:border-zinc-900'
+            required
+          />
+        </div>
 
-      <button
-        type='submit'
-        className='py-3 px-5 h-full rounded-md text-sm text-zinc-100 font-bold bg-red-500 transition-colors hover:bg-red-600'
-      >
-        Gerar Link
-      </button>
+        <button
+          type='submit'
+          disabled={isLoading}
+          className='flex items-center justify-center gap-2 py-3 px-5 h-full rounded-md text-sm text-zinc-100 font-bold bg-red-500 transition-colors hover:bg-red-600 disabled:cursor-not-allowed disabled:opacity-70'
+        >
+          { isLoading && <SpinnerGap size={20} className='animate-spin' /> }
+          Gerar Link
+        </button>
+      </div>
     </form>
   )
 };
